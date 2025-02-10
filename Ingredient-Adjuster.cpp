d80aedd57e625d7331f63356f68e85a31a399fcb
Brenna Meyer <brennameyer9@gmail.com>
2025-02-10 17:11:49 -0500

    Add project files.

---
+++ Ingredient-Adjuster.cpp
@@ -0,0 +1,18 @@
+// Ingredient-Adjuster.cpp : This file contains the 'main' function. Program execution begins and ends there.
+////Programmer:Brenna Meyer
+//Date: Febryary 10th,2025
+// Rerquirements:Ingredient Adjuster A cookie recipe calls for the following ingredients :
+//1.5 cups of sugar
+//1 cup of butter
+//2.75 cups of flour
+//The recipe produces 48 cookies with this amount of the ingredients.Write a program that asks the user 
+//how many cookies he or she wants to make, then displays the number of cups of each ingredient needed for the specified number of cookies.
+
+#include <iostream>
+
+int main()
+{
+   
+}
+
+
